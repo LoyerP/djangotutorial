dd8604388b7b0e69d221e184eb9c74b9b5f8404a
Rustled <157823270+LoyerP@users.noreply.github.com>
2026-06-09 10:38:44 -0400

    Add 'polls' app to INSTALLED_APPS
    Added custom app 'polls' to the INSTALLED_APPS list.

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Custom apps
+    'polls.apps.PollsConfig',
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
